ad3c3fde48e7221b5991a07b37414c087174a1fd
Vittor Valler <vittorvc@gmail.com>
2020-07-13 17:19:49 -0300

    Fixed statics

--- ariananutricionista/settings.py
+++ ariananutricionista/settings.py
@@ -144,12 +144,10 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'is-danger',
     messages.SUCCESS: 'is-success',
 }
-
-django_heroku.settings(locals())
