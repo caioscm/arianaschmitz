794bb2e40bb03830025b483ad6fa40a1e11b2408
Vittor Valler <vittorvc@gmail.com>
2020-07-14 13:37:35 -0300

    Static test

--- ariananutricionista/settings.py
+++ ariananutricionista/settings.py
@@ -142,6 +142,7 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+AWS_SECURE_URLS = False
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
