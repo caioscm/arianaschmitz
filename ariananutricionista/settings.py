37cc4cd47e3c783419511ad384d769dd7ab8b6fb
Vittor Valler <vittorvc@gmail.com>
2020-07-23 12:41:02 -0300

    Debugging

--- ariananutricionista/settings.py
+++ ariananutricionista/settings.py
@@ -154,3 +154,19 @@ MESSAGE_TAGS = {
 }
 
 django_heroku.settings(locals(), staticfiles=False)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}g
